ea886347a66cb911327b5a90d662db80471666b2
Dika Afty <dikaafty@gmail.com>
2025-05-23 00:54:23 +0700

    Import dehaze icon image

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import videobelajarLogo from "../assets/images/videobelajar-logo.png";
 import avatar from "../assets/images/avatar.png";
+import dehazeIcon from "../assets/images/dehaze.png";
 
 const Header = ({ isHomepageHeader = false }) => {
     
