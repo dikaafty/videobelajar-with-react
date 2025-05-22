d4dca0819b41444955d300985800c152c7ef3d0e
Dika Afty <dikaafty@gmail.com>
2025-05-23 01:10:58 +0700

    Fix dehaze icon import source error

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import videobelajarLogo from "../assets/images/videobelajar-logo.png";
 import avatar from "../assets/images/avatar.png";
-import dehazeIcon from "../assets/images/dehaze.png";
+import dehazeIcon from "../assets/images/dehaze-icon.png";
 
 const Header = ({ isHomepageHeader = false }) => {
   if(isHomepageHeader) {
