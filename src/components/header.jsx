783be80756c9d93ed68b16917fd98a9fe975c54b
Dika Afty <dikaafty@gmail.com>
2025-05-23 01:09:55 +0700

    Create jsx and its conditials in Header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,5 +3,43 @@ import avatar from "../assets/images/avatar.png";
 import dehazeIcon from "../assets/images/dehaze.png";
 
 const Header = ({ isHomepageHeader = false }) => {
-    
+  if(isHomepageHeader) {
+     return (
+     <div className="header">
+      <>
+        <div className="left">
+          <img 
+            src={videobelajarLogo}
+            alt="videobelajar logo"
+            className="videobelajarLogo"
+          />
+
+          <button className="navBtn">Kategori</button>
+
+          <img 
+            src={dehazeIcon}
+            alt="dehaze icon"
+            className="dehazeIcon"
+          />
+        </div>
+
+        <img 
+          src={avatar}
+          alt="avatar"
+          className="avatarImg"
+        />
+      </>
+    </div>
+    );
+  } else {
+    return (
+    <div className="header">
+       <img 
+        src={videobelajarLogo}
+        alt="videobelajar logo"
+        className="videobelajarLogo"
+      />
+    </div>
+    );
+  }
 }
