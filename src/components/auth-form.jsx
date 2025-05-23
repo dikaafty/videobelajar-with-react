2e29604c8546c997502942bca20a59a2c2c5f2a1
Dika Afty <dikaafty@gmail.com>
2025-05-23 22:32:17 +0700

    Create form sub title jsx in AuthForm component

--- src/components/auth-form.jsx
+++ src/components/auth-form.jsx
@@ -9,6 +9,10 @@ const AuthForm = ({ isLoginForm }) => {
             <h3 className="formTitle">
               {isLoginForm ? 'Masuk ke Akun' : 'Pendaftaran Akun'}
             </h3>
+            <p className="formSubTitle">
+              {isLoginForm ? 'Yuk, lanjutin belajarmu di videobelajar.' 
+              : 'Yuk, daftarkan akunmu sekarang juga!'}
+            </p>
           </div>
         </div>
       </>
