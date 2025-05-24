1bceeac0f251bdf310a1b447b5c691522b5d008f
Dika Afty <dikaafty@gmail.com>
2025-05-24 22:05:26 +0700

    Import Container component

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/header";
 import Body from "../components/body";
 import AuthForm from "../components/auth-form";
+import Container from "../components/container";
 
 const RegisterPage = () => {
   return (
