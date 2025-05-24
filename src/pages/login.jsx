8f589eba1366b8ec5edd49be3fac45398324e7bd
Dika Afty <dikaafty@gmail.com>
2025-05-24 18:20:54 +0700

    Import InputAction and BtnAction component

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,7 @@
 import Body from "../components/body";
 import AuthForm from "../components/auth-form";
+import InputAction from "../components/input-action";
+import BtnAction from "../components/btn-action";
 import IdenticalInput from "../components/identical-input";
 import ForgotPass from "../components/forgot-pass";
 import AuthBtn from "../components/auth-btn";
