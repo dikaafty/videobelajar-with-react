f2c5b2bdf73b4836540e623c369254947df29cd0
Dika Afty <dikaafty@gmail.com>
2025-05-24 18:21:49 +0700

    Import Divider component

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -5,6 +5,7 @@ import BtnAction from "../components/btn-action";
 import IdenticalInput from "../components/identical-input";
 import ForgotPass from "../components/forgot-pass";
 import AuthBtn from "../components/auth-btn";
+import Divider from "../components/divider";
 import GoogleSSOBtn from "../components/google-sso-btn";
 
 const LoginPage = () => {
