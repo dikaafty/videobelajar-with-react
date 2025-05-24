7741bab00c45066821d81b85d1699e8c9dd2328d
Dika Afty <dikaafty@gmail.com>
2025-05-24 13:47:09 +0700

    Create phone input container and its items jsx in PhoneInput component

--- src/components/phone-input.jsx
+++ src/components/phone-input.jsx
@@ -1,5 +1,38 @@
 const PhoneInput = () => {
   return (
-      
+    <div className="phoneInputContainer">
+      <label 
+        htmlFor="userPhone" 
+        className="identicalLabel">
+          No. Hp
+          <span className="asterisk">*</span>
+      </label>
+
+      <div className="phoneInputWrapper">
+        <div className="countryCode">
+          <div className="countryFlagContainer">
+            <img 
+              src="https://flagcdn.com/w40/id.png" 
+              alt="Indonesia Flag" 
+            />
+          </div>
+
+          <select>
+            <option 
+              value="+62" 
+              selected>
+                +62
+            </option>
+          </select>
+        </div>
+
+        <input 
+          type="tel" 
+          name="userPhone" 
+          id="userPhone" 
+          required 
+        />
+      </div>
+    </div>
   );
 }
