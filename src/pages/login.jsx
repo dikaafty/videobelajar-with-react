8a59fedc868505424175b2243b31dcad5aebce3f
Dika Afty <dikaafty@gmail.com>
2025-05-24 17:15:11 +0700

    Import IdenticalInput component

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,6 @@
 import Body from "../components/body";
 import AuthForm from "../components/auth-form";
+import IdenticalInput from "../components/identical-input";
 
 const Login = () => {
     return (
