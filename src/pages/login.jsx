9caa8d0f5e902b6d449642e76bf791ad268eb384
Dika Afty <dikaafty@gmail.com>
2025-05-24 17:16:33 +0700

    Import AuthBtn component

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,6 +2,7 @@ import Body from "../components/body";
 import AuthForm from "../components/auth-form";
 import IdenticalInput from "../components/identical-input";
 import ForgotPass from "../components/forgot-pass";
+import AuthBtn from "../components/auth-btn";
 
 const Login = () => {
     return (
