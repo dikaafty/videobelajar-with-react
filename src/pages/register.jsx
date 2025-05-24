7efb4544a24b61ef17d5ffa58536d19707f6c492
Dika Afty <dikaafty@gmail.com>
2025-05-24 22:09:04 +0700

    Import ForgotPass component

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -6,6 +6,7 @@ import ActionForm from "../components/action-form";
 import IdenticalInput from "../components/identical-input";
 import GenderInput from "../components/gender-input";
 import PhoneInput from "../components/phone-input";
+import ForgotPass from "../components/forgot-pass";
 
 const RegisterPage = () => {
   return (
