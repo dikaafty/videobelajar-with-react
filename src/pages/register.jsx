e1869a45c55973e43b91345d66b1b12d31bf308c
Dika Afty <dikaafty@gmail.com>
2025-05-24 22:08:04 +0700

    Import PhoneInput component

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -5,6 +5,7 @@ import Container from "../components/container";
 import ActionForm from "../components/action-form";
 import IdenticalInput from "../components/identical-input";
 import GenderInput from "../components/gender-input";
+import PhoneInput from "../components/phone-input";
 
 const RegisterPage = () => {
   return (
