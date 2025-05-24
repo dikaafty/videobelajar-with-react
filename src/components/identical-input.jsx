425502f03bc9e2de7080fde15a79b8cfb7dcb186
Dika Afty <dikaafty@gmail.com>
2025-05-24 12:56:13 +0700

    Create identical input container for non pass input and its items jsx in IdenticalInput component

--- src/components/identical-input.jsx
+++ src/components/identical-input.jsx
@@ -3,11 +3,24 @@ const IdenticalInput = (
 ) => {
   if(!isPassInput) {
     return (
-      
+      <div className="identicalInputContainer">
+        <label 
+          htmlFor={labelFor} 
+          className="identicalLabel">
+            {labelText}
+            <span className="asterisk">*</span>
+        </label>
+        <input 
+          type={inputType}
+          name={inputName}
+          id={inputId}
+          required
+        />
+      </div>
     );
   } else {
     return (
-
+      
     );
   }
 }
