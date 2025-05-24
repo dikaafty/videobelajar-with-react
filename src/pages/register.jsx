b0db83d135305c44d88afecb926aa9c033e5fef6
Dika Afty <dikaafty@gmail.com>
2025-05-24 22:06:24 +0700

    Import IdenticalInput component

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -3,6 +3,7 @@ import Body from "../components/body";
 import AuthForm from "../components/auth-form";
 import Container from "../components/container";
 import ActionForm from "../components/action-form";
+import IdenticalInput from "../components/identical-input";
 
 const RegisterPage = () => {
   return (
