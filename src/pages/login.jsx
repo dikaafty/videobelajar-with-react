d9fb16cef537d1d08873187eacc4bd0826975efb
Dika Afty <dikaafty@gmail.com>
2025-05-24 17:15:51 +0700

    Import ForgotPass component

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,7 @@
 import Body from "../components/body";
 import AuthForm from "../components/auth-form";
 import IdenticalInput from "../components/identical-input";
+import ForgotPass from "../components/forgot-pass";
 
 const Login = () => {
     return (
