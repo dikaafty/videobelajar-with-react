a6139463d7b934d293e88f919bce85620473ade8
Dika Afty <dikaafty@gmail.com>
2025-05-24 17:17:39 +0700

    Import GoogleSSOBtn component

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,6 +3,7 @@ import AuthForm from "../components/auth-form";
 import IdenticalInput from "../components/identical-input";
 import ForgotPass from "../components/forgot-pass";
 import AuthBtn from "../components/auth-btn";
+import GoogleSSOBtn from "../components/google-sso-btn";
 
 const Login = () => {
     return (
