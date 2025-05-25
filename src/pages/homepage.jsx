c4dc2ba5b7c53ad6b510513c9b0b947b60683715
Dika Afty <dikaafty@gmail.com>
2025-05-25 19:25:06 +0700

    Import Container component

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/header";
 import Body from "../components/body";
 import Hero from "../components/hero";
+import Container from "../components/container";
 import CardSectionDesc from "../components/card-section-desc";
 import CardSectionTabs from "../components/card-section-tabs";
 import Footer from "../components/footer";
