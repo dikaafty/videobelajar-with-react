9ca5703aa720e0179808fd866ab3f35ebafa4f6e
Dika Afty <dikaafty@gmail.com>
2025-05-25 18:40:02 +0700

    Create footer bottom bar and its items jsx in footerBottomBar component

--- src/components/footer-bottom-bar.jsx
+++ src/components/footer-bottom-bar.jsx
@@ -5,6 +5,33 @@ import twitterLogo from "../assets/images/twitter-logo.png";
 
 const footerBottomBar = () => {
   return (
-    
+    <div className="footerBottomBar">
+      <p 
+        className="copyrightText">
+          @2023 Gerobak Sayur All Rights Reserved.
+      </p>
+
+      <div className="brandSocials">
+        <img 
+          src={linkedInLogo} 
+          alt="linkedIn logo" 
+        />
+
+        <img 
+          src={facebookLogo} 
+          alt="facebook logo" 
+        />
+
+        <img 
+          src={instagramLogo} 
+          alt="instagram logo" 
+        />
+
+        <img 
+          src={twitterLogo} 
+          alt="twitter logo" 
+        />
+      </div>
+    </div>
   );
 }
