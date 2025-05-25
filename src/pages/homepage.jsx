0aef6c236c1c2a8de79ca72c9308205204e8425f
Dika Afty <dikaafty@gmail.com>
2025-05-25 19:20:38 +0700

    Import CardSectionTabs component

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,6 +2,7 @@ import Header from "../components/header";
 import Body from "../components/body";
 import Hero from "../components/hero";
 import CardSectionDesc from "../components/card-section-desc";
+import CardSectionTabs from "../components/card-section-tabs";
 import Footer from "../components/footer";
 
 const HomePage = () => {
