14c61752cff2db39ab1976126d41751e54909f16
Dika Afty <dikaafty@gmail.com>
2025-05-25 08:33:37 +0700

    Import all card images

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,3 +1,13 @@
+import cardImgOne from "../assets/images/card-image-one.jpeg";
+import cardImgTwo from "../assets/images/card-image-two.jpeg";
+import cardImgThree from "../assets/images/card-image-three.jpeg";
+import cardImgFour from "../assets/images/card-image-four.jpeg";
+import cardImgFive from "../assets/images/card-image-five.jpeg";
+import cardImgSix from "../assets/images/card-image-six.jpeg";
+import cardImgSeven from "../assets/images/card-image-seven.jpeg";
+import cardImgEight from "../assets/images/card-image-eight.jpeg";
+import cardImgNine from "../assets/images/card-image-nine.jpeg";
+
 const Card = ({ id }) => {
   return (
     <div className="card">
