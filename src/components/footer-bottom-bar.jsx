93b59abdd9b251d56c9858b8c1c43884e44a6a20
Dika Afty <dikaafty@gmail.com>
2025-05-25 18:33:47 +0700

    Import twitter logo

--- src/components/footer-bottom-bar.jsx
+++ src/components/footer-bottom-bar.jsx
@@ -1,3 +1,4 @@
 import linkedInLogo from "../assets/images/linkedin-logo.png";
 import facebookLogo from "../assets/images/facebook-logo.png";
 import instagramLogo from "../assets/images/instagram-logo.png";
+import twitterLogo from "../assets/images/twitter-logo.png";
