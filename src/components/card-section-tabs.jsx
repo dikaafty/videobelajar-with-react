50abba8e2df2b7691f78375409fe71ef8f2c81eb
Dika Afty <dikaafty@gmail.com>
2025-05-25 08:13:19 +0700

    Create card section tabs and its children jsx in CardSectionTabs component

--- src/components/card-section-tabs.jsx
+++ src/components/card-section-tabs.jsx
@@ -1,5 +1,42 @@
 const CardSectionTabs = () => {
   return (
-    
+    <div 
+      className="cardSectionTabs" 
+      role="tablist">
+        <button 
+          role="tab" 
+          aria-selected="true" 
+          className="tab active">
+            Semua Kelas
+        </button>
+
+        <button 
+          role="tab" 
+          aria-selected="false" 
+          className="tab">
+            Pemasaran
+        </button>
+
+        <button 
+          role="tab" 
+          aria-selected="false" 
+          className="tab">
+            Desain
+        </button>
+
+        <button 
+          role="tab" 
+          aria-selected="false" 
+          className="tab">
+            Pengembangan Diri
+        </button>
+
+        <button 
+          role="tab" 
+          aria-selected="false" 
+          className="tab">
+            Bisnis
+        </button>
+    </div>
   );
 }
