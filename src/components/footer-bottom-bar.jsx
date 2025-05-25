87e6ce7185f3852f259588443de8d000f66e18b2
Dika Afty <dikaafty@gmail.com>
2025-05-25 18:35:06 +0700

    Create footerBottomBar component

--- src/components/footer-bottom-bar.jsx
+++ src/components/footer-bottom-bar.jsx
@@ -2,3 +2,7 @@ import linkedInLogo from "../assets/images/linkedin-logo.png";
 import facebookLogo from "../assets/images/facebook-logo.png";
 import instagramLogo from "../assets/images/instagram-logo.png";
 import twitterLogo from "../assets/images/twitter-logo.png";
+
+const footerBottomBar = () => {
+  
+}
