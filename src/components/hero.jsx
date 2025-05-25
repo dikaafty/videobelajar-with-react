df313db78267502806e0b7fe655c394610c5e2f3
Dika Afty <dikaafty@gmail.com>
2025-05-25 07:52:59 +0700

    Create hero desc and its children jsx in Hero component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,7 +1,19 @@
 const Hero = () => {
   return (
     <div className="hero">
-      
+      <div className="heroDesc">
+        <h1 
+          className="heroTitle">
+            Revolusi Pembelajaran: Temukan Ilmu Baru melalui Platform Video Interaktif!
+        </h1>
+
+        <p 
+          className="heroSubTitle">
+            Temukan ilmu baru yang menarik dan mendalam melalui koleksi video pembelajaran
+            berkualitas tinggi. Tidak hanya itu, Anda juga dapat berpartisipasi dalam latihan
+            interaktif yang akan meningkatkan pemahaman Anda.
+        </p>
+      </div>
     </div>
   );
 }
