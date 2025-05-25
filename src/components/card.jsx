991f8e493650943ec48ddcd50bea7c9593a8404b
Dika Afty <dikaafty@gmail.com>
2025-05-25 08:37:23 +0700

    Import all char images

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -7,6 +7,14 @@ import cardImgSix from "../assets/images/card-image-six.jpeg";
 import cardImgSeven from "../assets/images/card-image-seven.jpeg";
 import cardImgEight from "../assets/images/card-image-eight.jpeg";
 import cardImgNine from "../assets/images/card-image-nine.jpeg";
+import charOne from "../assets/images/char-one.png";
+import charTwo from "../assets/images/char-two.png";
+import charThree from "../assets/images/char-three.png";
+import charFour from "../assets/images/char-four.png";
+import charFive from "../assets/images/char-five.png";
+import charSix from "../assets/images/char-six.png";
+import charSeven from "../assets/images/char-seven.png";
+import charEight from "../assets/images/char-eight.png";
 
 const Card = ({ id }) => {
   return (
