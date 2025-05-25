ee61362643fd609be4880bdf69697374d79084e4
Dika Afty <dikaafty@gmail.com>
2025-05-25 18:33:06 +0700

    Import instagram logo

--- src/components/footer-bottom-bar.jsx
+++ src/components/footer-bottom-bar.jsx
@@ -1,2 +1,3 @@
 import linkedInLogo from "../assets/images/linkedin-logo.png";
 import facebookLogo from "../assets/images/facebook-logo.png";
+import instagramLogo from "../assets/images/instagram-logo.png";
