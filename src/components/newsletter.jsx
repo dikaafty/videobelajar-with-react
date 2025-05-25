f1c1702d72c37e62e0e4d1bac4dd7504d01ca0be
Dika Afty <dikaafty@gmail.com>
2025-05-25 14:00:54 +0700

    Create newsletter and its items jsx in Newsletter component

--- src/components/newsletter.jsx
+++ src/components/newsletter.jsx
@@ -1,5 +1,25 @@
 const Newsletter = () => {
   return (
-    <div className="newsletterContent"></div>
+    <div className="newsletterContent">
+      <div className="newsletter">
+        <p 
+          className="newsletterText">
+            NEWSLETTER
+        </p>
+
+        <div className="newsletterDesc">
+          <h3 
+            className="newsletterTitle">
+              Mau Belajar Lebih Banyak?
+          </h3>
+
+          <p 
+            className="newsletterSubTitle">
+              Daftarkan dirimu untuk mendapatkan informasi terbaru dan penawaran
+              spesial dari program-program terbaik hariesok.id
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
