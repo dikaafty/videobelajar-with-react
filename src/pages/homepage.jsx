7244e7fb5beb9f30e8ad7050ee019ea9a81cef8f
Dika Afty <dikaafty@gmail.com>
2025-05-25 19:26:18 +0700

    Import Card component

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -4,6 +4,7 @@ import Hero from "../components/hero";
 import Container from "../components/container";
 import CardSectionDesc from "../components/card-section-desc";
 import CardSectionTabs from "../components/card-section-tabs";
+import Card from "../components/card";
 import Footer from "../components/footer";
 
 const HomePage = () => {
