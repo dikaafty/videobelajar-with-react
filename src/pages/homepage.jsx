b5b8b6a295ad69e6d3f768212392118911c85921
Dika Afty <dikaafty@gmail.com>
2025-05-25 19:20:04 +0700

    Import CardSectionDesc component

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/header";
 import Body from "../components/body";
 import Hero from "../components/hero";
+import CardSectionDesc from "../components/card-section-desc";
 import Footer from "../components/footer";
 
 const HomePage = () => {
