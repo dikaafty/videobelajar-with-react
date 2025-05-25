86ba8677bea025a1b1e7c166047b3744ab147b2b
Dika Afty <dikaafty@gmail.com>
2025-05-25 18:20:15 +0700

    Create nav and its items jsx in Nav component

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,5 +1,18 @@
 const Nav = ({ navText, navList = [] }) => {
   return (
-    
+    <div className="nav">
+      <p 
+        className="navText">
+          {navText}
+      </p>
+
+      <div className="navLists">
+        {
+          navList.length !== 0 && navList.map((nav) => (
+            <p>{nav}</p>
+          ))
+        }
+      </div>
+    </div>
   );
 }
