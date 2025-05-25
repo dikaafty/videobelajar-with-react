077c6eba19dd95a09a274535e2987ce8e94d9436
Dika Afty <dikaafty@gmail.com>
2025-05-25 14:24:43 +0700

    Create footer brand info and its items jsx in FooterBrandInfo component

--- src/components/footer-brand-info.jsx
+++ src/components/footer-brand-info.jsx
@@ -2,6 +2,28 @@ import videobelajarLogo from "../assets/images/videobelajar-logo.png";
 
 const FooterBrandInfo = () => {
   return (
-    
+    <div className="footerBrandInfo">
+      <img 
+        src={videobelajarLogo} 
+        alt="videobelajar logo" 
+      />
+
+      <div className="brandInfo">
+        <p 
+          className="textInfo">
+            Gali Potensi Anda Melalui Pembelajaran Video di hariesok.id!
+        </p>
+
+        <p 
+          className="brandAddress">
+            Jl. Usman Effendi No. 50 Lowokwaru, Malang
+        </p>
+
+        <p 
+          className="brandPhoneNumber">
+            +62-877-7123-1234
+        </p>
+      </div>
+    </div>
   );
 }
