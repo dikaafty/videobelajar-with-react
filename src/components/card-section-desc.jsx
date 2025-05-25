bfe7ad6124d1a2d75a0b1711083b71bee6daf9b6
Dika Afty <dikaafty@gmail.com>
2025-05-25 08:04:38 +0700

    Create card section desc and its children jsx in CardSectionDesc component

--- src/components/card-section-desc.jsx
+++ src/components/card-section-desc.jsx
@@ -1,5 +1,15 @@
 const CardSectionDesc = () => {
   return (
-    
+    <div className="cardSectionDesc">
+      <h3 
+        className="cardSectionTitle">
+          Koleksi Video Pembelajaran Unggulan
+      </h3>
+
+      <p 
+        className="cardSectionSubTitle">
+          Jelajahi Dunia Pengetahuan Melalui Pilihan Kami!
+      </p>
+    </div>
   );
 }
