08ed49768624f97d66b64a337c87276d01308ae7
Dika Afty <dikaafty@gmail.com>
2025-05-25 19:29:41 +0700

    Import Nav component

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -8,6 +8,7 @@ import Card from "../components/card";
 import Newsletter from "../components/newsletter";
 import Footer from "../components/footer";
 import FooterBrandInfo from "../components/footer-brand-info";
+import Nav from "../components/nav";
 
 const HomePage = () => {
   
